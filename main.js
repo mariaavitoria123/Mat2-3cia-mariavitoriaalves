b36cde765c57eb83d090dd93fd8325ed517caaf8
mariaa vitoria <110918829+mariaavitoria123@users.noreply.github.com>
2024-04-25 17:10:20 -0300

    2604

--- main.js
+++ main.js
@@ -15,5 +15,8 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
-contadores[0].textContent = "Contagem regressiva";
+contadores[0].textContent = tempoObjetivo1;
+
+
