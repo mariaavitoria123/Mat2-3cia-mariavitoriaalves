ea772843062719113209c15c415dc9be652725ec
mariaa vitoria <110918829+mariaavitoria123@users.noreply.github.com>
2024-05-02 17:39:30 -0300

    ttt

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
+for (let i = 0; i <botoes.length; i++){
     botoes[i].onclick = function(){
 
-        for(let j=0;j<botoes.length;j++){
+        for (let j = 0; j < botoes.length;j ++){
             botoes[j].classList.remove("ativo");
             botoes[j].classList.remove("ativo");
         }
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
