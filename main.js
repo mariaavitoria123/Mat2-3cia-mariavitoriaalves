ad3c2f08132b783f406b80a15a9e57ad349b722f
mariaa vitoria <110918829+mariaavitoria123@users.noreply.github.com>
2024-05-23 17:35:35 -0300

    2323

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1- tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjtivo - tempoAtual;
+    let segundos = tempoFinal/1000;
+    return segundos;
+
+}
